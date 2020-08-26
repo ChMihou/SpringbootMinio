239405a9fc9aa61aeba817f4c8b4a02f254119e7
Admin <rambo@mobplus.net>
2020-08-26 15:43:50 +0800

    Initial commit

---
+++ src/main/java/com/cmh/minio/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.cmh.minio.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("com.cmh.minio")).paths(PathSelectors.any()).build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("swagger-bootstrap-ui RESTful APIs").description("swagger-bootstrap-ui")
+                .termsOfServiceUrl("http://localhost:80/").contact("developer@mail.com").version("1.0").build();
+    }
+}
